320f1a0c06b6477f78a2d9f9c3d67470247399a2
teo-core <tsanchez@corenetworks.es>
2022-01-24 10:35:55 +0100

    estatic_method

--- OOP/Calificador/num_18_metodo_estatico.py
+++ OOP/Calificador/num_18_metodo_estatico.py
@@ -9,16 +9,16 @@ class Calificaciones():
     def __init__(self,alumno_notas=[]) -> None:
 
         if alumno_notas:
-            self.nombre = alumno_notas[0]
-            self.notas = alumno_notas[1:]
-            self.calificacion = self.calcula_calificacion()
+            self.__nombre = alumno_notas[0]
+            self.__notas = alumno_notas[1:]
+            self.__calificacion = self.calcula_calificacion()
         else:
-            self.nombre = ''
-            self.notas = []
-            self.calificacion = ''
+            self.__nombre = ''
+            self.__notas = []
+            self.__calificacion = ''
     
     def __str__(self) -> str:
-        return f'Alumno: {self.nombre} tiene la calificación {self.calificacion}'
+        return f'Alumno: {self.__nombre} tiene la calificación {self.__calificacion}'
 
     def calcula_calificacion(self):
         calificacion = ''
@@ -38,3 +38,70 @@ class Calificaciones():
             calificacion = None
         
         return calificacion
+    
+    # def alumno_sin_registro(self, nuevo_alumno):
+    #     if self.nombre == '':
+    #         self.nombre = nuevo_alumno
+    #     return self.nombre
+
+
+    # def set_alumno(self, alum):
+    #     self.nombre = alum[0]
+    #     for elem in alum[1:]:
+    #         self.notas.append(elem)
+    #     self.calificacion = self.calcula_calificacion()
+
+    @property
+    def alumno(self):
+        return self.__nombre
+
+    @alumno.setter
+    def alumno(self,nuevo_alumno):
+        self.__nombre = nuevo_alumno
+
+    
+    @property
+    def notas(self):
+        return self.__notas
+
+    @notas.setter
+    def notas(self, nuevas_notas):
+        self.__notas = nuevas_notas
+        self.__calificacion = self.calcula_calificacion()
+    
+    @property
+    def calificacion(self):
+        return self.__calificacion
+
+    @staticmethod
+    def valida_notas(lista_notas):
+        """
+        Devuelve True cuando:
+            - Cada nota esté entre 0-10
+            - El tipo debe ser int o float
+            - La lista no debe estar vacía
+        """
+        valido = True
+        if lista_notas == []:
+            valido = False
+
+        for nota in lista_notas:
+            if not type(nota) in (int,float) or not (0.0<= nota <= 10.0) :
+                valido = False
+        
+        return valido
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- OOP/Calificador/test_calificador.py
+++ OOP/Calificador/test_calificador.py
@@ -8,12 +8,12 @@ class TestCalificaciones(ut.TestCase):
 
     def test_constructor_vacio_propiedades_vacias(self):
         cal = Calificaciones()
-        self.assertEqual(cal.nombre, '')
+        self.assertEqual(cal.alumno, '')
         self.assertEqual(cal.notas, [])
 
     def test_constructor_recibe_valores_establece_props(self):
         cal = Calificaciones(['Raul',9.2,5,4.5,7,9.1])
-        self.assertEqual(cal.nombre, 'Raul')
+        self.assertEqual(cal.alumno, 'Raul')
         self.assertEqual(cal.notas, [9.2,5,4.5,7,9.1])
 
     def test_metodo_str_devuelve_alumno_calificacion(self):
@@ -30,5 +30,34 @@ class TestCalificaciones(ut.TestCase):
         self.assertEqual(cal.calcula_calificacion(),'BIEN')
         self.assertEqual(cal.calificacion,'BIEN')
 
-    
+    # def test_alumno_no_inicializado_asignar_nombre_calif(self):
+    #     cal = Calificaciones()
+    #     cal.set_alumno(['Pedro',9.7])
+    #     self.assertEqual(cal.nombre,'Pedro')
+    #     self.assertEqual(cal.notas,[9.7])
+    #     self.assertEqual(cal.calificacion,'SOBRESALIENTE')
 
+    def test_asigna_valores_alumno_nulo(self):
+        cal = Calificaciones()
+        cal.nombre = 'Paco'
+        self.assertEqual(cal.nombre,'Paco')
+
+    
+    def test_asigna_notas(self):
+        cal = Calificaciones()
+        cal.notas = [5,6,7,8,9,10]
+
+        self.assertEqual(cal.notas,[5,6,7,8,9,10])
+        self.assertEqual(cal.calificacion,'NOTABLE')    
+
+    def test_validar_notas_incorrectas(self):
+        lista_notas = [1,22]
+        self.assertEqual(Calificaciones.valida_notas(lista_notas),False)
+
+    def test_validar_notas_incorrectas_cadena(self):
+        lista_notas = [1,'hola']
+        self.assertEqual(Calificaciones.valida_notas(lista_notas),False)
+
+    def test_validar_notas_correctas(self):
+        lista_notas = [5,6.2,7,8,9,10]
+        self.assertEqual(Calificaciones.valida_notas(lista_notas),True)
